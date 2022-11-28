3a18ad9e03d898ff5f24482440dec9953d8be683
Saniya Mulla <shaikhsaniya701@gmail.com>
2022-11-28 18:39:01 +0530

    changed js files to jsx

--- src/index.js
+++ src/index.js
@@ -1,17 +1,40 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+import React from 'react';  
+import ReactDOM from 'react-dom';  
+import { BrowserRouter as Router, Route, Link, NavLink, Switch } from 'react-router-dom'  
+import './index.css';  
+import App from './App';  
+import About from './about'  
+import Contact from './contact'  
+// import Notfound from './notfound'  
+  
+const routing = (  
+  <Router>  
+    <div>  
+      <h1>React Router Example</h1>  
+      <ul>  
+        <li>  
+          <NavLink to="/" exact activeStyle={  
+             {color:'red'}  
+          }>Home</NavLink>  
+        </li>  
+        <li>  
+          <NavLink to="/about" exact activeStyle={  
+             {color:'green'}  
+          }>About</NavLink>  
+        </li>  
+        <li>  
+          <NavLink to="/contact" exact activeStyle={  
+             {color:'magenta'}  
+          }>Contact</NavLink>  
+        </li>  
+      </ul>  
+      
+         <Route exact path="/" component={App} />  
+         <Route path="/about" component={About} />  
+         <Route path="/contact" component={Contact} />  
+         {/* <Route component={Notfound} />   */}
+     
+    </div>  
+  </Router>  
+)  
+ReactDOM.render(routing, document.getElementById('root'));  
